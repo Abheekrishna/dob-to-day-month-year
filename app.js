a26b3cca90ba9820c98398fb7431c20e48db63e1
Abheekrishna <abheekrishna17@gmail.com>
2023-05-28 10:58:41 +0530

    FIXED: styles

--- app.js
+++ app.js
@@ -4,20 +4,23 @@ const dobPlaceholder = document.getElementById('dobPlaceholder')
 
 
 const extractAge = (dob) => {
-    const age = dob.split('-')
+    const age = dob.split(' ')
     const day = parseInt(age[0], 10);
     const month = parseInt(age[1], 10);
     const year = parseInt(age[2], 10)
     const dobDate = new Date (year, month - 1, day)
     const dayOfWeek = dobDate.getDay()
     const dayOfMonth = dobDate.getUTCMonth()
+    const currentDate = new Date()
+    const currentYear = currentDate.getFullYear()
+    const currentAge = currentYear - year;
 
     if (day <= 31 && month <= 12) {
     const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const dayName = dayNames[dayOfWeek]
     const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const monthName = monthNames[dayOfMonth]
-    dobPlaceholder.innerText = `You were born on ${dayName}, ${day}th ${monthName} ${year}`
+    dobPlaceholder.innerText = `"You were born on ${dayName}, ${day}th ${monthName} ${year} and let me guess you're ${currentAge} right?"`
 
 } else if (day > 31 || month > 12) {
     dobPlaceholder.innerText = 'Are a drunk or what? Enter DOB properly!'
